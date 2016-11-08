67d1dadeb9d2c50293ecafc5f93286d46bb11435
cristian giron <cristiangiron1993@gmail.com>
2016-11-08 16:20:15 -0500

    actualizaciontodo

---
+++ public/javascripts/app.js
@@ -0,0 +1,8 @@
+ var app= angular.module('Angularapp', []);
+    // app.controller('BooksController',BooksController);
+
+    // app.controller('BooksController',function($scope){
+    //     $scope.book = book;
+    // $scope.msg ='Este campo es obligatorio';
+
+    // });
---
+++ public/javascripts/jsonBooks.js
@@ -0,0 +1,47 @@
+var book = [
+    {
+        "year": "1967",
+        "name": "Cien años de soledad",
+        "autor": "Gabriel Garcia",
+        "descripcion": "Cien años de soledad es una novela del escritor colombiano Gabriel García Márquez, ganador del Premio Nobel de Literatura en 1982",
+        "price": 5,
+        "canPurchase": false,
+        "agotado": true
+    },
+    {
+        "year": " 1304 ",
+        "name": "La Divina comedia",
+        "autor": "Dante Alighieri",
+        "descripcion": "es un poema escrito por Dante Alighieri. Se desconoce la fecha exacta en que fue escrito aunque las opiniones más reconocidas aseguran que el Infierno pudo ser compuesto entre 1304 y 1307 o 1308, el Purgatorio de 1307 o 1308 a 1313 o 1314 y por último, el Paraíso de 1313 o 1314 a 1321, fecha del fallecimiento del poeta. Libro más famoso de su autor, es una de las obras fundamentales de la transición del pensamiento medieval (teocentrista) al renacentista (antropocentrista). Es considerada la obra maestra de la literatura italiana y una de las cumbres de la",
+        "price": 5,
+        "canPurchase": false,
+        "agotado": true
+    },
+    {
+        "year": " non ullamcorper velit sem at magna. ",
+        "name": ", scelerisque in diam sit amet, dignissim ",
+        "autor": "varius elit sed",
+        "descripcion": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus fermentum nisl, auctor consectetur odio pulvinar efficitur. Phasellus nulla ligula, vulputate non ultrices eu",
+        "price": 5,
+        "canPurchase": false,
+        "agotado": false
+    },
+    {
+        "year": " Curabitur pulvinar malesuada ",
+        "name": "varius elit sed",
+        "autor": "uis arcu luctus, varius eli",
+        "descripcion": "Phasellus iaculis dignissim mattis. Fusce a suscipit mi, eget rhoncus lectus. Aenean pellentesque feugiat maximus. Suspendisse nunc dui, suscipit nec diam id, elementum varius odio. In hac habitasse platea dictumst. Sed sollicitudin tincidunt dignissim. Nulla lobortis lectus vel iaculis hendrerit.",
+        "price": 5,
+        "canPurchase": false,
+        "agotado": false
+    },
+    {
+        "year": " eget rhoncus lectus ",
+        "name": "ut dignissim orci commodo",
+        "autor": "Vestibulum varius arcu quis",
+        "descripcion": "Phasellus iaculis dignissim mattis. Fusce a suscipit mi, eget rhoncus lectus. Aenean pellentesque feugiat maximus. Suspendisse nunc dui, suscipit nec diam id, elementum varius odio. In hac habitasse platea dictumst. Sed sollicitudin tincidunt dignissim. Nulla lobortis lectus vel iaculis hendrerit.",
+        "price": 5,
+        "canPurchase": false,
+        "agotado": false
+    },
+]
